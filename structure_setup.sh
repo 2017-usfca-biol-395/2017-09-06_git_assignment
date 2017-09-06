349b7a3ac19033f22c2257250627adc7a6b474bb
Allan Nojadera <acnojadera2@dons.usfca.edu>
2017-09-06 15:29:51 -0700

    Added code to write .gitkeep files to script structure_setup.sh

--- structure_setup.sh
+++ structure_setup.sh
@@ -6,6 +6,18 @@
 echo "Creating folders now"
 mkdir code data data/raw_data output output/figures output/tables
 
+echo "Creating .gitkeep files in empty directories"
+cd code
+touch .gitkeep
+cd ..
+cd output/figures
+touch .gitkeep
+cd ..
+cd tables
+touch .gitkeep
+cd ..
+cd ..
+
 echo "Downloading fasta file"
 cd data/raw_data
 curl -L http://npk.io/aMPCOe+ -o crab_rRNA.fasta
