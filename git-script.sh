f99ae894ff1f109f6761506346c5376e1b95c6fc
Rigo Enriquez <rdenriquez@dons.usfca.edu>
2017-09-06 15:56:29 -0700

    initial commit of script

---
+++ git-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+# a script to practice using git  
+# Rigo Enriquez rdenriquez@dons.usfca.edu 
+# September 9, 2017 
+
+# Set up folder structure 
+echo "Creating folder structure..."
+mkdir code data data/raw_data output output/figures output/tables 
+
+touch code/.gitkeep output/.gitkeep
+
+# Download fasta file into raw data 
+echo "Downloading data..." 
+curl -L http://npk.io/aMPCOe+ -o data/raw_data/crab_rRNA.fasta 
+
+echo "All done!"
+
